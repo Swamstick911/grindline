bce0a234d25e59f18b6fc33023ef6f0d806fc836
Swamstick911 <163744064+Swamstick911@users.noreply.github.com>
2025-09-30 12:52:38 +0530

    Update PomodoroPage.jsx

--- src/components/PomodoroPage.jsx
+++ src/components/PomodoroPage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { motion, AnimatePresence } from "framer-motion";
 import MagneticButton from "./MagneticButton";
-import { useLocalStorage } from "../hooks/useLocalStorage";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const PomodoroPage = () => {
   const [workMinutes, setWorkMinutes] = useState(() => {
@@ -253,6 +253,7 @@ const PomodoroPage = () => {
 };
 
 export default PomodoroPage;
+
 
 
 
