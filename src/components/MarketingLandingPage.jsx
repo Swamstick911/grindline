33ccd3f9165ae1b9309e7e787da669f268037306
Swamstick911 <163744064+Swamstick911@users.noreply.github.com>
2025-09-30 11:58:55 +0530

    Update MarketingLandingPage.jsx

--- src/components/MarketingLandingPage.jsx
+++ src/components/MarketingLandingPage.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import MagneticButton from "./MagneticButton";
-import useLocalStorage from "../hooks/useLocalStorage";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 export default function MarketingLandingPage() {
   const navigate = useNavigate();
@@ -66,4 +66,5 @@ export default function MarketingLandingPage() {
     </div>
   );
 }
+
 
